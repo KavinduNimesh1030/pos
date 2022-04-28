08e8a1e79446cf14eed1d8f84317194d266f82e8
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2022-04-28 18:34:04 +0530

    Refacotored Save Item and Delete item

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -1,30 +1,46 @@
 package dao;
 
 import db.DBConnection;
-import model.CustomerDTO;
 import model.ItemDTO;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ItemDAOImpl {
-    public ArrayList<ItemDTO>loadAllItems() throws SQLException, ClassNotFoundException {
+    public ArrayList<ItemDTO> loadAllItems() throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         Statement stm = connection.createStatement();
         ResultSet rst = stm.executeQuery("SELECT * FROM Item");
 
-        ArrayList<ItemDTO>allItems = new ArrayList<>();
-        while (rst.next()){
+        ArrayList<ItemDTO> allItems = new ArrayList<>();
+        System.out.println("Run");
+        while (rst.next()) {
             String id = rst.getString(1);
             String dec = rst.getString(2);
-            BigDecimal unitPrice = BigDecimal.valueOf(rst.getDouble(3));
-            int qty = Integer.parseInt(rst.getString(4));
-            allItems.add(new ItemDTO(id,dec,unitPrice,qty));
+            int qty = Integer.parseInt(rst.getString(3));
+            BigDecimal unitPrice = BigDecimal.valueOf(rst.getDouble(4));
+            allItems.add(new ItemDTO(id, dec, unitPrice, qty));
         }
         return allItems;
     }
+
+    public boolean saveItem(ItemDTO item) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+        pstm.setString(1, item.getCode());
+        pstm.setString(2, item.getDescription());
+        pstm.setBigDecimal(3, item.getUnitPrice());
+        pstm.setInt(4, item.getQtyOnHand());
+        return pstm.executeUpdate() > 0;
+    }
+
+    public void deleteItem(String code) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Item WHERE code=?");
+        pstm.setString(1, code);
+        pstm.executeUpdate();
+
+    }
+
 }
