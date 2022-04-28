dbac5e4f4b0f4dce0c5473b59aaec847927539b7
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2022-04-28 18:45:32 +0530

    Refectored update item

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -42,5 +42,14 @@ public class ItemDAOImpl {
         pstm.executeUpdate();
 
     }
+    public void updateItem(ItemDTO item) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Item SET description=?, unitPrice=?, qtyOnHand=? WHERE code=?");
+        pstm.setString(1, item.getDescription());
+        pstm.setBigDecimal(2, item.getUnitPrice());
+        pstm.setInt(3, item.getQtyOnHand());
+        pstm.setString(4, item.getCode());
+        pstm.executeUpdate();
+    }
 
 }
